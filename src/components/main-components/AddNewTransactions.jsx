db18fad36490ea45f0b5873f9a180d5c741f8f4e
Amira Azeer <amira@linearsix.com>
2023-07-17 11:36:49 +0530

    Iniital Commit

--- src/components/main-components/AddNewTransactions.jsx
+++ src/components/main-components/AddNewTransactions.jsx
@@ -1,21 +1,32 @@
-import React, { useState, useContext } from 'react'
+import React, { useContext, useRef } from 'react'
 import { FormControl, TextField, Button } from '@mui/material'
 import { GlobalStateContext } from "../../context/GlobalState.js";
 
 
 const AddNewTransactions = () => {
-  const [ expenditure, setExpenditure ] = useState('')
-  const [ amount, setAmount ] = useState('')
+  // const [ expenditure, setExpenditure ] = useState('')
+  // const [ amount, setAmount ] = useState('')
+
+  const expenditureTypeInputRef = useRef()
+  const amountInputRef = useRef()
+
 
   const { addTransaction } = useContext(GlobalStateContext);
 
-  function onSubmit(){
+  const onSubmit = (event) => {
+    event.preventDefault()
+    // console.log(expenditureTypeInputRef.current.value)
+   
     const addNewTransaction =  {
       "id" :  Math.floor(Math.random() * 100000000),
-      "expenditure": expenditure,
-      "value": parseInt(amount) // + sign makes it into a number
+      "expenditure": expenditureTypeInputRef.current.value,
+      "value": parseInt(amountInputRef.current.value) // + sign makes it into a number
     }
     addTransaction(addNewTransaction);
+    
+    // Manipulating DOM elements or you can work with state to reset
+    expenditureTypeInputRef.current.value=''
+    amountInputRef.current.value=''
   }
 
   return (
@@ -23,11 +34,17 @@ const AddNewTransactions = () => {
       <h2>Add a new Transaction</h2>
       
       <FormControl sx={{ display:'flex', flexDirection:'row', }} >
-        <TextField sx={{ m:'10px' }} label="Expenditure Type" variant="outlined" onChange={(e) => setExpenditure(e.target.value)}/>
-        <TextField sx={{ m:'10px' }} label="Enter Cost" variant="outlined" onChange={(e) => setAmount(e.target.value)} />
+        <TextField sx={{ m:'10px' }} inputRef={expenditureTypeInputRef} label="Expenditure Type" variant="outlined" 
+          // onChange={(e) => setExpenditure(e.target.value)}
+        />
+        <TextField sx={{ m:'10px' }} inputRef={amountInputRef} label="Enter Cost" variant="outlined" 
+          // onChange={(e) => setAmount(e.target.value)} 
+        />
+    
       </FormControl>
 
-      <Button sx={{ width:'90%', m:'20px' }} variant='contained' onClick={() => onSubmit()}> Add Transaction </Button>
+      <Button sx={{ width:'90%', m:'20px' }} variant='contained' type="submit" onClick={(event) => onSubmit(event)} > Add Transaction </Button>
+
 
       <p>Note : Negative - Expense & Positive - Income</p>
     </>
